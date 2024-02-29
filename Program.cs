4461ebba13a143f501b25766d32bf777a41b457e
binooka96 <binookabino@gmail.com>
2024-03-01 02:52:12 +0530

    1st program

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System; 
+  
+// namespace declaration 
+namespace Main { 
+      
+    // Class declaration 
+    class Program { 
+          
+        // Main Method 
+        static void Main(string[] args) { 
+              
+            // statement 
+            // printing Hello World! 
+            Console.WriteLine("Binooka"); 
+              
+            // To prevents the screen from  
+            // running and closing quickly 
+            Console.ReadKey(); 
+        } 
+    } 
+} 
